ee8ecffb940fed7fe82881e5978d8fb2c942ba37
José Raphael Santos do Bú <jose.raphael@opus-software.com.br>
2021-02-09 16:59:50 -0300

    projeto inicialiado

---
+++ index.js
@@ -0,0 +1,9 @@
+const { request, response } = require('express');
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({message: "server is up"});
+});
+
+app.listen(3333);
